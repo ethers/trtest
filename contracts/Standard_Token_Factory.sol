1d30964ed6b1dd8f3d5637f33f2d2e72bda0b8da
ethers <ethereum@outlook.com>
2015-09-09 12:53:32 -0700

    factory call works

--- contracts/Standard_Token_Factory.sol
+++ contracts/Standard_Token_Factory.sol
@@ -2,10 +2,11 @@ import "Standard_Token";
 
 contract Standard_Token_Factory {
 
-    function createStandardToken() returns (address) {
+    function createStandardToken() returns (uint) {
 
-        address newTokenAddr = address(new Standard_Token());
-        Standard_Token newToken = Standard_Token(newTokenAddr);
-        return newTokenAddr;
+        /*address newTokenAddr = address(new Standard_Token());
+        Standard_Token newToken = Standard_Token(newTokenAddr);*/
+        return 99;
+        /*return newTokenAddr;*/
     }
 }
--- test/example.js
+++ test/example.js
@@ -1,36 +1,36 @@
 contract("Standard_Token_Factory", function(accounts) {
 
-  // it("passes", function(done) {
-  //   Standard_Token.new(10000, {from: accounts[0]}).then(function(ctr) {
-  //       return ctr.coinBalanceOf.call(accounts[0]);
-  //   }).then(function (result) {
-  //       //console.log(result);
-  //       assert.strictEqual(result.c[0], 10000);
-  //       done();
-  //   }).catch(done);
-  // });
+  it("factory call", function(done) {
+      var factory = Standard_Token_Factory.at(Standard_Token_Factory.deployed_address);
+      factory.createStandardToken.call()
+      .then(function (result) {
+          console.log('RESULT', result);
+          assert.strictEqual(result.c[0], 99);
+          done();
+      }).catch(done);
+  });
 
   it("caller creates new token and has correct initial balance", function(done) {
       var factory = Standard_Token_Factory.at(Standard_Token_Factory.deployed_address);
       factory.createStandardToken({from: accounts[0]})
       .then(function(tokenContractAddr) {
-          console.log('@@@ addr: ', tokenContractAddr)
+          console.log('tokenContractAddr: ', tokenContractAddr)
           var newToken = Standard_Token.at(tokenContractAddr, {gas: 3141592, gasLimit: 3141592});
 
-          return newToken.foo.call();
+          return newToken.foo.call();  // Error: invalid address ??
       }).then(function (result) {
-          console.log(result);
+          console.log('RESULT', result);
           assert.strictEqual(result.c[0], 15);
           done();
       }).catch(done);
   });
 
 
-  it("calls foo", function(done) {
+  it("calls foo.  this passes", function(done) {
     Standard_Token.new({from: accounts[0]}).then(function(ctr) {
         return ctr.foo.call();
     }).then(function (result) {
-        //console.log(result);
+        // console.log('RESULT', result);
         assert.strictEqual(result.c[0], 15);
         done();
     }).catch(done);
