66c1aa2b4e29060e64503ced1f849d88673f3bed
ethers <ethereum@outlook.com>
2015-09-09 12:17:11 -0700

    Error: invalid address when doing newToken.coinBalance.call()
         Error: invalid address
        at maybeWrapAsError (node_modules/bluebird/js/main/util.js:70:12)
        at Function.ret [as call] (<anonymous>:30:41)
        at trtest/test/example.js:24:41

--- test/example.js
+++ test/example.js
@@ -1,13 +1,37 @@
 contract("Standard_Token_Factory", function(accounts) {
 
-  it("passes", function(done) {
-    Standard_Token.new(10000, {from: accounts[0]}).then(function(ctr) {
-        return ctr.coinBalanceOf.call(accounts[0]);
-    }).then(function (result) {
-        //console.log(result);
-        assert.strictEqual(result.c[0], 10000);
-        done();
-    }).catch(done);
-  });
+  // it("passes", function(done) {
+  //   Standard_Token.new(10000, {from: accounts[0]}).then(function(ctr) {
+  //       return ctr.coinBalanceOf.call(accounts[0]);
+  //   }).then(function (result) {
+  //       //console.log(result);
+  //       assert.strictEqual(result.c[0], 10000);
+  //       done();
+  //   }).catch(done);
+  // });
 
+  it("caller creates new token and has correct initial balance", function(done) {
+      var factory = Standard_Token_Factory.at(Standard_Token_Factory.deployed_address);
+      factory.createStandardToken(10000, {from: accounts[0]})
+      .then(function(tokenContractAddr) {
+          console.log('@@@ addr: ', tokenContractAddr)
+          var newToken = Standard_Token.at(tokenContractAddr, {gas: 3141592, gasLimit: 3141592});
+
+          // console.log('@@@ NT: ', newToken)
+          console.log('@@@ coinbal: ', newToken.coinBalanceOf.call)
+
+
+          return newToken.coinBalance.call();
+
+          // return newToken.coinBalanceOf.call(accounts[0]);
+
+          // return newToken.coinBalanceOf.call(tokenContractAddr);
+          // console.log('@@@ NT: ', newToken)
+          // return newToken.coinBalance.call();
+      }).then(function (result) {
+          console.log(result);
+          assert.strictEqual(result.c[0], 10000);
+          done();
+      }).catch(done);
+  });
 });
