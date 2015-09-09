e47d08c712919d1826b4bf4eade12489422f6b1f
ethers <ethereum@outlook.com>
2015-09-09 12:43:12 -0700

    simplify

--- contracts/Standard_Token_Factory.sol
+++ contracts/Standard_Token_Factory.sol
@@ -2,9 +2,9 @@ import "Standard_Token";
 
 contract Standard_Token_Factory {
 
-    function createStandardToken(uint256 _initialAmount) returns (address) {
+    function createStandardToken() returns (address) {
 
-        address newTokenAddr = address(new Standard_Token(_initialAmount));
+        address newTokenAddr = address(new Standard_Token());
         Standard_Token newToken = Standard_Token(newTokenAddr);
         return newTokenAddr;
     }
--- test/example.js
+++ test/example.js
@@ -12,32 +12,22 @@ contract("Standard_Token_Factory", function(accounts) {
 
   it("caller creates new token and has correct initial balance", function(done) {
       var factory = Standard_Token_Factory.at(Standard_Token_Factory.deployed_address);
-      factory.createStandardToken(10000, {from: accounts[0]})
+      factory.createStandardToken({from: accounts[0]})
       .then(function(tokenContractAddr) {
           console.log('@@@ addr: ', tokenContractAddr)
           var newToken = Standard_Token.at(tokenContractAddr, {gas: 3141592, gasLimit: 3141592});
 
-          // console.log('@@@ NT: ', newToken)
-          console.log('@@@ coinbal: ', newToken.coinBalanceOf.call)
-
-
           return newToken.foo.call();
-
-          // return newToken.coinBalanceOf.call(accounts[0]);
-
-          // return newToken.coinBalanceOf.call(tokenContractAddr);
-          // console.log('@@@ NT: ', newToken)
-          // return newToken.coinBalance.call();
       }).then(function (result) {
           console.log(result);
-          assert.strictEqual(result.c[0], 10000);
+          assert.strictEqual(result.c[0], 15);
           done();
       }).catch(done);
   });
 
 
   it("calls foo", function(done) {
-    Standard_Token.new(10000, {from: accounts[0]}).then(function(ctr) {
+    Standard_Token.new({from: accounts[0]}).then(function(ctr) {
         return ctr.foo.call();
     }).then(function (result) {
         //console.log(result);
