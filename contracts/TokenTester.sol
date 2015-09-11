b2a02a45ecf50784d6ef56a03b7b10d13709759d
ethers <ethereum@outlook.com>
2015-09-10 17:02:41 -0700

    tester writes to storage; need to resolve TRACE:eth.vm.exitEXCEPTION available=0 needed=14 cause=INSUFFICIENT STACK op=DUP14

--- contracts/TokenTester.sol
+++ contracts/TokenTester.sol
@@ -2,13 +2,17 @@ import "Standard_Token";
 import "Standard_Token_Factory";
 
 contract TokenTester {
+    address tokenContractAddress;
 
-    function getTokenContractAddress() returns (uint) {
+    function writeTokenContractAddress() {
         address factoryAddr = address(new Standard_Token_Factory());
         Standard_Token_Factory tokenFactory = Standard_Token_Factory(factoryAddr);
 
         address tcAddr = tokenFactory.makeT();
-        Standard_Token tokenContract = Standard_Token(tcAddr);
-        return tokenContract.foo();
+
+        tokenContractAddress = tcAddr;
+
+        /*Standard_Token tokenContract = Standard_Token(tcAddr);
+        return tokenContract.foo();*/
     }
 }
--- test/tokenTester.js
+++ test/tokenTester.js
@@ -2,10 +2,15 @@ contract("TokenTester", function(accounts) {
 
   it("TODO", function(done) {
       var tester = TokenTester.at(TokenTester.deployed_address);
-      // tester.getTokenContractAddress({from: accounts[0]})
-      tester.getTokenContractAddress.call({gas: 3141592, gasLimit: 3141592})
-      .then(function (result) {
-          console.log('RESULT', result);
+      tester.writeTokenContractAddress({from: accounts[0]})
+      .then(function (tx) {
+          console.log('tx', tx);
+
+          var testerAddr = TokenTester.deployed_address;
+          var tcAddr = web3.eth.getStorageAt(testerAddr, 0);
+
+          console.log('tcAddr: ', tcAddr)
+
           // assert.strictEqual(result.c[0], 99);
           done();
       }).catch(done);
